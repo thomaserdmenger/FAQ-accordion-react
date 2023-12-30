46055d1db5ab4ae27dff33dd0cae1e30bec59326
thomaserdmenger <thomas.erdmenger@googlemail.com>
2023-12-30 17:52:11 +0100

    Add FAQ data and component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import * as React from 'react';
+import Accordion from './components/Accordion';
+import styles from './App.module.css';
+
 export default function App() {
-  return null;
+  return <Accordion />;
 }
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import FAQ from './FAQ';
+
+export default function () {
+  return <FAQ />;
+}
