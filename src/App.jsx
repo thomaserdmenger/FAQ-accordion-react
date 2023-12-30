000b93fe3a8f94ca13d3ccbe93c26f22904c8cf2
thomaserdmenger <thomas.erdmenger@googlemail.com>
2023-12-30 21:54:52 +0100

    Add final styles to mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import Accordion from './components/Accordion';
 import styles from './App.module.css';
+import iconStar from './assets/images/icon-star.svg';
 
 export default function App() {
   return (
     <main>
-      <section>
+      <section className={styles.cardWrapper}>
+        <div className={styles.cardHeadingWrapper}>
+          <img
+            src={iconStar}
+            alt='Star Logo'
+          />
+          <h1>FAQs</h1>
+        </div>
         <Accordion />
       </section>
     </main>
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import data from '../assets/data/data';
 import styles from '../App.module.css';
+import iconMinus from '../assets/images/icon-minus.svg';
+import iconPlus from '../assets/images/icon-plus.svg';
 
 export default function ({ id, answer, question }) {
   const [accordionOpen, setAccordionOpen] = React.useState(false);
@@ -10,16 +12,31 @@ export default function ({ id, answer, question }) {
   };
 
   return (
-    <div key={id}>
+    <div
+      className={styles.faqWrapper}
+      key={id}
+    >
       <div
         className={styles.faqHeader}
         onClick={toggleState}
       >
         <h2>{question}</h2>
-        {accordionOpen ? <span>-</span> : <span>+</span>}
+        {accordionOpen ? (
+          <img
+            className={styles.image}
+            src={iconMinus}
+            alt='Icon Minus'
+          />
+        ) : (
+          <img
+            className={styles.image}
+            src={iconPlus}
+            alt='Icon Plus'
+          />
+        )}
       </div>
       <div className={accordionOpen ? styles.faqBodyHide : styles.faqBody}>
-        <p>{answer}</p>
+        <p className={styles.faqAnswer}>{answer}</p>
       </div>
     </div>
   );
